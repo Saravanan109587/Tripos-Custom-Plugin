c7e006d6cfbc8d5c2d476e98d9312ca23c54f514
Saravanan N <saravanan109587@gmail.com>
2019-04-09 13:11:58 +0530

    js Export type changed

--- www/tripos.js
+++ www/tripos.js
@@ -1,7 +1,13 @@
 /*global cordova, module*/
-var TriposModule={
-    greet: function (name, successCallback, errorCallback) {
+
+var TriposModule=function(){};
+ 
+   TriposModule.greet= function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Tripos", "greet", [name]);
     }
-}
+
+    TriposModule.greet2= function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Tripos", "greet", [name]);
+    }
+ 
 module.exports =  TriposModule;
