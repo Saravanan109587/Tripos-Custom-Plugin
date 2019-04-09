492ea1ae6072e1d4531250e0610fa8c1e72fe4b5
Saravanan N <saravanan109587@gmail.com>
2019-04-09 12:56:24 +0530

    Changed from Hellow to TriPOs

--- www/tripos.js
+++ www/tripos.js
@@ -1,7 +1,7 @@
 /*global cordova, module*/
 var TriposModule={
     greet: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
+        cordova.exec(successCallback, errorCallback, "Tripos", "greet", [name]);
     }
 }
 module.exports =  TriposModule;
